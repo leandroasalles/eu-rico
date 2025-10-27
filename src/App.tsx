e066d5455a9f792e793f2d08453b63c3c9b851cd
Leandro Salles <leandro.asalles182@gmail.com>
2025-10-27 16:30:16 -0300

    Feat: Error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+  },
+  {
+    path: "*",
+    element: <Error />,
   },
 ]);
 
---
+++ src/pages/Error/index.tsx
@@ -0,0 +1,17 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+function Error() {
+  return (
+    <div className="w-full h-svh">
+      <div>
+        <DotLottieReact
+          src="https://lottie.host/a623b4fc-ec35-404e-8c13-edf1d52d701f/Mf8MQKetPu.lottie"
+          loop
+          autoplay
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Error;
