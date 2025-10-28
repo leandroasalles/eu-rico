d0cc85d98319a21e25c891fa21c888bd85e19d2c
Leandro Salles <leandro.asalles182@gmail.com>
2025-10-28 17:44:08 -0300

    Feat: Create resume cards for home screen

---
+++ src/components/Cards/resumeCard/index.tsx
@@ -0,0 +1,17 @@
+interface ResumeCardProps {
+  title: string;
+  value: number;
+}
+
+function ResumeCard({ title, value }: ResumeCardProps) {
+  return (
+    <div className="bg-white h-fit w-2xs my-10 rounded-sm border-primary-red border-2 p-4 flex flex-col gap-2">
+      <p className="font-semibold">{title}</p>
+      <p className={`text-xl ${value > 0 ? "text-green-500" : "text-red-500"}`}>
+        R$ {value.toFixed(2)}
+      </p>
+    </div>
+  );
+}
+
+export default ResumeCard;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,34 @@
 import Header from "../../components/Header";
 import SideMenu from "../../components/sideMenu";
+import ResumeCard from "../../components/Cards/resumeCard";
+import { useState, useEffect } from "react";
 
 function Home() {
+  const [saldo, setSaldo] = useState(0);
+  const [receitas, setReceitas] = useState(500);
+  const [despesas, setDespesas] = useState(-100);
+
+  useEffect(() => {
+    handleSaldo();
+  }, [receitas, despesas]);
+
+  function handleSaldo() {
+    const saldo = [receitas, despesas];
+    const saldoTotal = saldo.reduce((acc, curr) => acc + curr, 0);
+    setSaldo(saldoTotal);
+  }
+
   return (
-    <div className="w-full h-svh flex flex-col">
+    <div className="w-full h-svh flex flex-col bg-background">
       <Header />
-      <SideMenu />
+      <div className="flex w-full h-full">
+        <SideMenu />
+        <div className="w-full flex justify-evenly">
+          <ResumeCard title="Saldo atual" value={saldo} />
+          <ResumeCard title="Receitas" value={receitas} />
+          <ResumeCard title="Despesas" value={despesas} />
+        </div>
+      </div>
     </div>
   );
 }
